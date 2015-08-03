cb1cd34196f2a4e225c4264a4da3195ae6ab58d3
Tal Wrii <talwrii@googlemail.com>
2015-08-03 22:44:04 +0100

    setup.py -- make this installable

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "xpm",
+    version = "0.0.4",
+    author = "Tal Wrii",
+    author_email = "talwrii@gmail.com",
+    description = "Library to output XPM files",
+    license = "BSD",
+    keywords = "xpm image PIL graphics",
+    url = "https://github.com/talwrii/nm-noauto",
+    py_modules=['xpm'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Graphics",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
