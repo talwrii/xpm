bdd6d39a8d07e157fa22b979d828493c991b36bc
Tal Wrii <talwrii@googlemail.com>
2015-08-03 22:40:57 +0100

    get everything working with python3
    Not worth it :P. The inability to do string interpolation
    on byte arrays is a real pain... although it
    appears to have been fixed on 3.5

--- xpm.py
+++ xpm.py
@@ -2,26 +2,27 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import sys
 import collections
 import itertools
 import math
 import string
 
 
-def pil_save(pil_image, variable_name='picture'):
+def pil_save(pil_image, variable_name=b'picture'):
     formatter = XpmImage.from_pil(pil_image, variable_name)
     return formatter.make_image()
 
-COLOUR_DIGITS = string.digits + string.ascii_lowercase
+COLOUR_DIGITS = (string.digits + string.ascii_lowercase).encode('utf8')
 
 class XpmImage(object):
     @classmethod
-    def from_pil(cls, pil_image, variable_name='picture'):
+    def from_pil(cls, pil_image, variable_name=b'picture'):
         pil_image = pil_image.convert('RGB')
         transparent_colour = pil_image.info.get('transparency')
         return cls(pil_image.size, pil_image.load(), variable_name, transparent_colour)
 
-    def __init__(self, size, pixels, variable_name='picture', transparent_colour=None):
+    def __init__(self, size, pixels, variable_name=b'picture', transparent_colour=None):
         "Pixels is a dictionary mapping x,y coordinates to rgba tuples"
         self.xsize, self.ysize = size
         self.variable_name = variable_name
@@ -41,38 +42,39 @@ class XpmImage(object):
             colours.remove(self.transparent_colour)
             colours = [self.transparent_colour] + colours
 
-        PRETTY_NAMES = ' X+.|/'
+        PRETTY_NAMES = u' X+.|/'
 
         for colour, char in zip(colours[:len(PRETTY_NAMES)], PRETTY_NAMES):
-            self.colour_table[colour] = char * self.colour_width
+
+            self.colour_table[colour] = char.encode('ascii') * self.colour_width
 
 
     def make_header(self):
-        return '\n'.join([
+        return b'\n'.join([
             b"/* XPM */",
-            b"static char* {}[] = {{".format(self.variable_name),
+            "static char* {}[] = {{".format(self.variable_name.decode('ascii')).encode('ascii'),
             b""])
 
     def make_footer(self):
         return b'};'
 
     def make_info(self):
-        return [b'"{} {} {} {}"'.format(self.xsize, self.ysize, len(self.colours), 1)]
+        return ['"{} {} {} {}"'.format(self.xsize, self.ysize, len(self.colours), 1).encode('ascii')]
 
     def make_colour_table(self):
         for colour in self.colours:
             yield self.make_colour_entry(colour)
 
     def make_colour_entry(self, colour):
-        return b'"{}\tc {}"'.format(
-            self.colour_table[colour],
-            self.format_colour(colour))
+        return '"{}\tc {}"'.format(
+            self.colour_table[colour].decode('ascii'),
+            self.format_colour(colour)).encode('ascii')
 
     def format_colour(self, colour):
         if colour == self.transparent_colour:
             return 'None'
         else:
-            return b'#{}{}{}'.format(*['{:02x}'.format(c) for c in colour])
+            return '#{}{}{}'.format(*['{:02x}'.format(c) for c in colour]).encode('ascii')
 
     def make_pixels(self):
         for y in range(self.ysize):
@@ -93,7 +95,7 @@ class XpmImage(object):
 
 def enumerate_colours(width):
     for x in itertools.product(*((COLOUR_DIGITS,) * width)):
-        yield b''.join(x)
+        yield bytes(x)
 
 if __name__ == '__main__':
     import PIL.Image
@@ -104,4 +106,9 @@ if __name__ == '__main__':
     PARSER.add_argument('--variable-name', '-v', type=str, help='XPM files are valid C code creating a variable representing an image. The name of this variable', default='picture')
     args = PARSER.parse_args()
     image = PIL.Image.open(args.file)
-    print(pil_save(image, variable_name=args.variable_name))
+    xpm_bytes = pil_save(image, variable_name=args.variable_name.encode('ascii'))
+
+    if sys.version_info.major >= 3:
+        sys.stdout.buffer.write(xpm_bytes)
+    else:
+        sys.stdout.write(xpm_bytes)
