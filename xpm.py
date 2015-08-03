fe9c5cf3d35cb7e49eda996a6cf8de4150fe750c
Tal Wrii <talwrii@googlemail.com>
2015-08-03 22:49:45 +0100

    add some beauty to whitespace

--- xpm.py
+++ xpm.py
@@ -8,12 +8,12 @@ import itertools
 import math
 import string
 
+COLOUR_DIGITS = (string.digits + string.ascii_lowercase).encode('utf8')
 
 def pil_save(pil_image, variable_name=b'picture'):
     formatter = XpmImage.from_pil(pil_image, variable_name)
     return formatter.make_image()
 
-COLOUR_DIGITS = (string.digits + string.ascii_lowercase).encode('utf8')
 
 class XpmImage(object):
     @classmethod
@@ -28,14 +28,29 @@ class XpmImage(object):
         self.variable_name = variable_name
         self.pixels = pixels
         self.transparent_colour = transparent_colour
-        colour_counts = collections.Counter(self.pixels[x, y] for x in range(self.xsize) for y in range(self.ysize))
+        colour_counts = collections.Counter(
+            self.pixels[x, y]
+            for x in range(self.xsize)
+            for y in range(self.ysize))
 
-        self.colours = [x for x, _ in sorted(colour_counts.items(), key=lambda x: -x[1])]
+        self.colours = [x
+            for x, _ in sorted(
+                colour_counts.items(), key=lambda x: -x[1])]
+
         self.colour_width = int(
             math.log(len(self.colours)) //
             math.log(len(COLOUR_DIGITS))) + 1
-        self.colour_table = dict(zip(self.colours, enumerate_colours(self.colour_width)))
 
+        self.colour_table = dict(
+            zip(
+                self.colours,
+                enumerate_colours(self.colour_width)))
+
+        self.add_pretty_colour_names()
+
+    def add_pretty_colour_names(self):
+        """Simple and varying characters
+        make it a lot easier to see an image"""
         colours = list(self.colours)
 
         if self.transparent_colour:
@@ -44,22 +59,27 @@ class XpmImage(object):
 
         PRETTY_NAMES = u' X+.|/'
 
-        for colour, char in zip(colours[:len(PRETTY_NAMES)], PRETTY_NAMES):
-
-            self.colour_table[colour] = char.encode('ascii') * self.colour_width
+        for colour, char in zip(
+                colours[:len(PRETTY_NAMES)],
+                PRETTY_NAMES):
+            self.colour_table[colour] = (
+                char.encode('ascii') * self.colour_width)
 
 
     def make_header(self):
         return b'\n'.join([
             b"/* XPM */",
-            "static char* {}[] = {{".format(self.variable_name.decode('ascii')).encode('ascii'),
+            "static char* {}[] = {{".format(
+                self.variable_name.decode('ascii')).encode('ascii'),
             b""])
 
     def make_footer(self):
         return b'};'
 
     def make_info(self):
-        return ['"{} {} {} {}"'.format(self.xsize, self.ysize, len(self.colours), 1).encode('ascii')]
+        return ['"{} {} {} {}"'.format(
+            self.xsize, self.ysize,
+            len(self.colours), 1).encode('ascii')]
 
     def make_colour_table(self):
         for colour in self.colours:
@@ -74,7 +94,9 @@ class XpmImage(object):
         if colour == self.transparent_colour:
             return 'None'
         else:
-            return '#{}{}{}'.format(*['{:02x}'.format(c) for c in colour]).encode('ascii')
+            return '#{}{}{}'.format(
+                *['{:02x}'.format(c) for c in colour]
+                ).encode('ascii')
 
     def make_pixels(self):
         for y in range(self.ysize):
@@ -101,12 +123,16 @@ if __name__ == '__main__':
     import PIL.Image
     import argparse
 
-    PARSER = argparse.ArgumentParser(description='Turn an image into an XPM assuming PIL can open it')
+    PARSER = argparse.ArgumentParser(description=
+        'Turn an image into an XPM assuming PIL can open it')
     PARSER.add_argument('file', type=str, help='file to convert')
-    PARSER.add_argument('--variable-name', '-v', type=str, help='XPM files are valid C code creating a variable representing an image. The name of this variable', default='picture')
+    PARSER.add_argument('--variable-name', '-v', type=str,
+        help='XPM files are valid C code creating a variable representing an image. The name of this variable', default='picture')
     args = PARSER.parse_args()
     image = PIL.Image.open(args.file)
-    xpm_bytes = pil_save(image, variable_name=args.variable_name.encode('ascii'))
+    xpm_bytes = pil_save(
+        image,
+        variable_name=args.variable_name.encode('ascii'))
 
     if sys.version_info.major >= 3:
         sys.stdout.buffer.write(xpm_bytes)
